adba7eb51ad96099c211e207502650685ab494ff
kdmmva <arzu879bayramov@mail.ru>
2024-06-30 14:38:22 +0400

    add app

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import Auth from './components/Auth';
+
+const App = () => {
+  return (
+    <div>
+      <Auth />
+    </div>
+  );
+};
+
+export default App;
