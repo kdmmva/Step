0232d6746b39bc49a3c9918c088d1f625c320f23
arzu <arzu879bayramov@mail.ru>
2025-01-28 11:26:57 +0400

    update fileManager.js

--- node.js_hw1/fileManager.js
+++ node.js_hw1/fileManager.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 
-// Чтение файла
 const readFile = (filePath) => {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf-8', (err, data) => {
@@ -13,7 +12,6 @@ const readFile = (filePath) => {
   });
 };
 
-// Запись в файл
 const writeFile = (filePath, content) => {
   return new Promise((resolve, reject) => {
     fs.writeFile(filePath, content, 'utf-8', (err) => {
